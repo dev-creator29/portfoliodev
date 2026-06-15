3cfc1f31a1fe60f4fa23f9763d4475b6cf081139
Devansh aggarwal <87365181+dev-creator29@users.noreply.github.com>
2026-06-15 14:50:52 +0530

    intial

---
+++ script.js
@@ -0,0 +1,21 @@
+window.addEventListener("scroll", () => {
+
+    const cards = document.querySelectorAll(".card, .project-card");
+
+    cards.forEach(card => {
+
+        const top = card.getBoundingClientRect().top;
+
+        if (top < window.innerHeight - 100) {
+            card.style.opacity = "1";
+            card.style.transform = "translateY(0)";
+        }
+    });
+
+});
+
+document.querySelectorAll(".card, .project-card").forEach(card => {
+    card.style.opacity = "0";
+    card.style.transform = "translateY(30px)";
+    card.style.transition = "0.6s ease";
+});
